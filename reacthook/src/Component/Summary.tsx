e182ec9b20c3af0ad25b06cea8981f811bd80a54
c0ncentus <jeremymartin33610@gmail.com>
2021-10-03 17:45:51 +0200

    reacy

---
+++ reacthook/src/Component/Summary.tsx
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+const Summary = (props:any) => {
+  return (
+    <div className="summary">
+      <h1>{props.name}</h1>
+      <p>
+        Gender: <span className="summary__output">{props.gender}</span>
+      </p>
+      <p>
+        Height: <span className="summary__output">{props.height}</span>
+      </p>
+      <p>
+        Hair Color / Skin Color:{' '}
+        <span className="summary__output">{props.hairColor}</span> /{' '}
+        <span className="summary__output">{props.skinColor}</span>
+      </p>
+      <p>
+        Appears in # Movies:{' '}
+        <span className="summary__output">{props.movieCount}</span>
+      </p>
+    </div>
+  );
+};
+
+export default Summary;
